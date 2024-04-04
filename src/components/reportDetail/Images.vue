f0e565f37d99b8490357fa1b127de7dbc194203d
sukranyorulmazzzz <sukran_yrlmz@hotmail.com>
2024-04-04 12:38:05 +0300

    .

--- src/components/reportDetail/Images.vue
+++ src/components/reportDetail/Images.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="column-gap-24px-div">
-    <img class="selected-report-images" :src="selectedImageSrc" />
-    <div class="yeni">
+    <img
+      class="selected-report-images"
+      :src="selectedImageSrc"
+      @click="showPopup"
+    />
+
+    <div v-if="popupVisible" class="image-popup" @click="closePopup">
+      <div class="popup-content">
+        <img class="popup-image" :src="selectedImageSrc" />
+      </div>
+    </div>
+
+    <div
+      class="row-gap-24px-div report-images-div"
+      :style="{ width: selectedImageWidth + 'px' }"
+    >
       <div
-        class="row-gap-24px-div report-images-div"
-        :style="{ width: selectedImageWidth + 'px' }"
+        v-for="(image, index) in imageData"
+        :key="index"
+        @click="selectImage(index)"
       >
-        <div
-          v-for="(image, index) in imageData"
-          :key="index"
-          @click="selectImage(index)"
-        >
-          <img class="report-images" :src="`data:image/png;base64, ${image}`" />
-        </div>
+        <img class="report-images" :src="`data:image/png;base64, ${image}`" />
       </div>
     </div>
   </div>
@@ -28,8 +37,9 @@ export default {
   },
   data() {
     return {
-      selectedImageIndex: null,
+      selectedImageIndex: 0,
       selectedImageWidth: 0,
+      popupVisible: false,
     };
   },
   computed: {
@@ -37,33 +47,48 @@ export default {
       return this.selectedImageIndex !== null &&
         this.imageData[this.selectedImageIndex]
         ? `data:image/png;base64, ${this.imageData[this.selectedImageIndex]}`
-        : `data:image/png;base64, ${this.imageData[0]}`;
+        : "";
+    },
+    previousImageSrc() {
+      const previousIndex =
+        this.selectedImageIndex > 0
+          ? this.selectedImageIndex - 1
+          : this.imageData.length - 1;
+      return `data:image/png;base64, ${this.imageData[previousIndex]}`;
+    },
+    nextImageSrc() {
+      const nextIndex =
+        this.selectedImageIndex < this.imageData.length - 1
+          ? this.selectedImageIndex + 1
+          : 0;
+      return `data:image/png;base64, ${this.imageData[nextIndex]}`;
     },
   },
   methods: {
     selectImage(index) {
       this.selectedImageIndex = index;
     },
-  },
-  mounted() {
-    this.imageData.forEach((image, index) => {
-      console.log(`Image ${index + 1} data:`, image);
-      console.log(`Image ${index + 1} base64 content:`, image.src);
-    });
-    const calculateWidth = () => {
+    showPopup() {
+      this.popupVisible = true;
+    },
+    closePopup() {
+      this.popupVisible = false;
+    },
+    calculateWidth() {
       const selectedReportImages = document.querySelector(
         ".selected-report-images"
       );
       if (selectedReportImages) {
-        const width = selectedReportImages.offsetWidth;
-        if (selectedReportImages) {
-          this.selectedImageWidth = selectedReportImages.offsetWidth;
-        }
+        this.selectedImageWidth = selectedReportImages.offsetWidth;
       }
-    };
-
-    calculateWidth();
-    window.addEventListener("resize", calculateWidth);
+    },
+  },
+  mounted() {
+    this.calculateWidth();
+    window.addEventListener("resize", this.calculateWidth);
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.calculateWidth);
   },
 };
 </script>
@@ -87,9 +112,30 @@ export default {
 .report-images-div {
   padding-top: 24px;
 }
-@media (max-width: 1000px) {
-  .selected-report-images {
-    height: 260px;
-  }
+
+.image-popup {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.popup-content {
+  position: relative;
+  max-width: 80%;
+  max-height: 80%;
+  overflow: auto;
+}
+
+.popup-image {
+  max-width: 100%;
+  max-height: 100%;
+  border-radius: 6px;
+  object-fit: contain;
 }
 </style>
