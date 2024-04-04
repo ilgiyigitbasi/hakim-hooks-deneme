bd1b4baa08657ee36f6c1d9d60288bc7857705b3
sukranyorulmazzzz <sukran_yrlmz@hotmail.com>
2024-04-04 11:44:04 +0300

    .

--- src/components/dashboard/DashboardListItem.vue
+++ src/components/dashboard/DashboardListItem.vue
@@ -5,9 +5,14 @@
     </div>
 
     <div class="list-item-dashboard">
-      <p class="explanation-text-black license-plate">
-        {{ listData.license_plate }}
-      </p>
+      <router-link
+        class="license-plate-router"
+        :to="{ path: '/vehicleHistory', query: { id: listData.license_plate } }"
+      >
+        <p class="explanation-text-black license-plate">
+          {{ formattedLicensePlate }}
+        </p>
+      </router-link>
     </div>
 
     <div class="list-item-dashboard">
@@ -30,7 +35,7 @@
 
       <router-link
         :to="{ path: '/reportDetail', query: { id: listData.id } }"
-        class="detail-button"
+        class="license-plate-router"
       >
         <img src="../../assets/img/gotodetail.svg" alt="" />
       </router-link>
@@ -45,9 +50,14 @@
 
       <div class="row-gap-16px-div">
         <p class="explanation-text item-text">License Plate:</p>
-        <p class="explanation-text-black license-plate">
-          {{ listData.license_plate }}
-        </p>
+
+        <router-link
+          :to="{ path: '/vehicleHistory', query: { id: licensePlate } }"
+        >
+          <p class="explanation-text-black license-plate">
+            {{ formattedLicensePlate }}
+          </p>
+        </router-link>
       </div>
     </div>
   </div>
@@ -77,9 +87,40 @@ export default {
       return `${formattedDate} ${formattedTime}`;
     },
   },
+  computed: {
+    formattedLicensePlate() {
+      // Lisans plakası verisini al
+      const licensePlate = this.listData.license_plate;
+
+      // Eğer plaka verisi boşsa veya belirtilen değerlerden biriyle eşleşiyorsa "not found" döndür
+      if (
+        !licensePlate ||
+        ["plaka bulunamadı", "Plaka bulunamadı", "Plaka Bulunamadı"].includes(
+          licensePlate
+        )
+      ) {
+        return "None";
+      }
+
+      // Plaka verisi uygunsa aynı şekilde geri döndür
+      return licensePlate;
+    },
+  },
 };
 </script>
 
 <style scoped>
 @import "../../assets/css/styles.css";
+.license-plate {
+  cursor: pointer;
+  text-decoration: none;
+  transition: all 0.15s;
+}
+.license-plate:hover {
+  border: 1px solid #3382f8;
+  color: #3382f8;
+}
+.license-plate-router {
+  text-decoration: none;
+}
 </style>
