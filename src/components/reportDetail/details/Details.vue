3633ac46673f466e0e657989de8855ab8ad3da26
sukranyorulmazzzz <sukran_yrlmz@hotmail.com>
2024-04-04 13:00:25 +0300

    .

--- src/components/reportDetail/details/Details.vue
+++ src/components/reportDetail/details/Details.vue
@@ -43,17 +43,7 @@ export default {
   },
   methods: {
     formatDateTime(dateTime) {
-      const date = new Date(dateTime);
-      const formattedDate = date.toLocaleDateString("en-US", {
-        year: "numeric",
-        month: "short",
-        day: "2-digit",
-      });
-      const formattedTime = date.toLocaleTimeString("en-US", {
-        hour: "2-digit",
-        minute: "2-digit",
-      });
-      return `${formattedDate} ${formattedTime}`;
+      return dateTime.slice(0, -7);
     },
   },
 };
